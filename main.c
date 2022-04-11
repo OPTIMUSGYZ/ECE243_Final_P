1048298d5417f0e31c62431dc8d72a2c940a4326
OPTIMUS_GYZ <optimus.gyz@gmail.com>
2022-04-11 11:11:05 -0400

    bullets working; canDie;

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
+int drawing = 1;
+int canDie = 0;
 #define BOARD                 "DE1-SoC"
 
 /* Memory */
@@ -85,6 +87,7 @@
 /* Screen size. */
 #define RESOLUTION_X 320
 #define RESOLUTION_Y 240
+#define DIAGONAL_LENGTH 400
 
 /* Constants for animation */
 #define BOX_LEN 2
@@ -103,7 +106,8 @@
 
 
 volatile int pixel_buffer_start; // global variable
-volatile short int display[RESOLUTION_Y][RESOLUTION_X];
+volatile int displayFront[RESOLUTION_Y][RESOLUTION_X];
+volatile int displayBack[RESOLUTION_Y][RESOLUTION_X];
 // 2 sets of drewPixel for front and back buffer
 volatile int drewPixelBack[RESOLUTION_X * RESOLUTION_Y][2];
 volatile int drewPixelFront[RESOLUTION_X * RESOLUTION_Y][2];
@@ -116,6 +120,9 @@ volatile int ndrewPixelBack = 0;
 volatile int ndrewPixelFront = 0;
 
 volatile int dead = 0;
+volatile int count = 0;
+
+int bulletSize = cannonSize - 1;
 
 void swap(int *a, int *b);
 
@@ -137,13 +144,18 @@ void drawPlayer(int x, int y);
 
 void drawBullet(int x, int y, int appear);
 
-int drawing = 1;
+void computeBulletPath(int bulletX, int bulletY, int playerX, int playerY, int bulletIdx,
+                       int bulletPathX[NUM_CANNONS][DIAGONAL_LENGTH],
+                       int bulletPathY[NUM_CANNONS][DIAGONAL_LENGTH], int speed);
+
+int countOccurances(int arr[], int n, int item);
 
 int main(void) {
     volatile int *pixel_ctrl_ptr;
     if (drawing)
         pixel_ctrl_ptr = (int *) PIXEL_BUF_CTRL_BASE;
-    int i;
+    int i, j, k;
+    int remainingBullets = 0;
     /*
          *     c4   c5   c6
          *
@@ -162,8 +174,57 @@ int main(void) {
                                 RESOLUTION_Y - 1, RESOLUTION_Y - 1, RESOLUTION_Y - 1};
     //int cannonYes[10] = {TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE};
     int cannonYes[NUM_CANNONS] = {TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE};
-    int cannonDirections[NUM_CANNONS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int d[NUM_CANNONS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int cannonDirections[NUM_CANNONS];
+    int bulletYes[NUM_CANNONS] = {cannonYes[0], cannonYes[1], cannonYes[2], cannonYes[3], cannonYes[4], cannonYes[5],
+                                  cannonYes[6], cannonYes[7], cannonYes[8], cannonYes[9]};
+    int initBulletX[NUM_CANNONS] = {cannonX[0], cannonX[1],
+
+                                    cannonX[2] - bulletSize + 1, cannonX[3] - bulletSize + 1,
+
+                                    cannonX[4] + cannonSize - bulletSize / 2 + 1,
+                                    cannonX[5] + cannonSize - bulletSize / 2 + 1,
+                                    cannonX[6] + cannonSize - bulletSize / 2 + 1,
+
+                                    cannonX[7] + cannonSize - bulletSize / 2 + 1,
+                                    cannonX[8] + cannonSize - bulletSize / 2 + 1,
+                                    cannonX[9] + cannonSize - bulletSize / 2 + 1};
+    int initBulletY[NUM_CANNONS] = {cannonY[0] + cannonSize - bulletSize / 2 + 1,
+                                    cannonY[1] + cannonSize - bulletSize / 2 + 1,
+
+                                    cannonY[2] + cannonSize - bulletSize / 2 + 1,
+                                    cannonY[3] + cannonSize - bulletSize / 2 + 1,
+
+                                    cannonY[4], cannonY[5], cannonY[6],
+
+                                    cannonY[7] - bulletSize + 1, cannonY[8] - bulletSize + 1,
+                                    cannonY[9] - bulletSize + 1};
+    int bulletX[NUM_CANNONS] = {cannonX[0], cannonX[1],
+
+                                cannonX[2] - bulletSize + 1, cannonX[3] - bulletSize + 1,
+
+                                cannonX[4] + cannonSize - bulletSize / 2 + 1,
+                                cannonX[5] + cannonSize - bulletSize / 2 + 1,
+                                cannonX[6] + cannonSize - bulletSize / 2 + 1,
+
+                                cannonX[7] + cannonSize - bulletSize / 2 + 1,
+                                cannonX[8] + cannonSize - bulletSize / 2 + 1,
+                                cannonX[9] + cannonSize - bulletSize / 2 + 1};
+    int bulletY[NUM_CANNONS] = {cannonY[0] + cannonSize - bulletSize / 2 + 1,
+                                cannonY[1] + cannonSize - bulletSize / 2 + 1,
+
+                                cannonY[2] + cannonSize - bulletSize / 2 + 1,
+                                cannonY[3] + cannonSize - bulletSize / 2 + 1,
+
+                                cannonY[4], cannonY[5], cannonY[6],
+
+                                cannonY[7] - bulletSize + 1, cannonY[8] - bulletSize + 1, cannonY[9] - bulletSize + 1};
+    int xPathBullet[NUM_CANNONS][DIAGONAL_LENGTH], yPathBullet[NUM_CANNONS][DIAGONAL_LENGTH];
+    for (i = 0; i < NUM_CANNONS; i++) {
+        for (j = 0; j < DIAGONAL_LENGTH; j++) {
+            xPathBullet[i][j] = -1;
+            yPathBullet[i][j] = -1;
+        }
+    }
 
     /* set front pixel buffer to start of FPGA On-chip memory */
     if (drawing)
@@ -189,18 +250,58 @@ int main(void) {
         clear_screen();
         drawPlayer(playerX, playerY);
         for (i = 0; i < NUM_CANNONS; i++) {
-            if (i < 4) {
-                cannonDirections[i] =
-                        atan(((double) (playerY - cannonY[i]) / (double) (playerX - cannonX[i]))) / M_PI * 180;
+            if (cannonYes[i]) {
+                if (i < 4) {
+                    cannonDirections[i] =
+                            atan(((double) (playerY - cannonY[i]) / (double) (playerX - cannonX[i]))) / M_PI * 180;
+                } else {
+                    cannonDirections[i] =
+                            atan(((double) (playerX - cannonX[i]) / (double) (playerY - cannonY[i]))) / M_PI * 180;
+                }
+                if (cannonDirections[i] > 63) {
+                    cannonDirections[i] = 63;
+                } else if (cannonDirections[i] < -63) {
+                    cannonDirections[i] = -63;
+                }
+                if (!remainingBullets)
+                    computeBulletPath(bulletX[i], bulletY[i], playerX, playerY, i, xPathBullet, yPathBullet, 2);
+            }
+        }
+        if (!drawing) {
+            FILE *g = fopen("test1.txt", "w");
+            for (i = 0; i < NUM_CANNONS; i++) {
+                fprintf(g, "%d: ", i);
+                for (j = 0; j < DIAGONAL_LENGTH; j++) {
+                    if (xPathBullet[i][j] != -1 && yPathBullet[i][j] != -1)
+                        fprintf(g, "(%d,%d) ", xPathBullet[i][j], yPathBullet[i][j]);
+                }
+                fprintf(g, "\n\n");
+            }
+        }
+
+        for (i = 0; i < NUM_CANNONS; i++) {
+            drawBullet(bulletX[i], bulletY[i], bulletYes[i]);
+            if (xPathBullet[i][count] != -1 && yPathBullet[i][count] != -1) {
+                bulletX[i] = xPathBullet[i][count];
+                bulletY[i] = yPathBullet[i][count];
             } else {
-                cannonDirections[i] =
-                        atan(((double) (playerX - cannonX[i]) / (double) (playerY - cannonY[i]))) / M_PI * 180;
+                bulletYes[i] = 0;
             }
-            if (cannonDirections[i] > 63) {
-                cannonDirections[i] = 63;
-            } else if (cannonDirections[i] < -63) {
-                cannonDirections[i] = -63;
+        }
+        remainingBullets = countOccurances(bulletYes, NUM_CANNONS, TRUE);
+        if (remainingBullets) {
+            count++;
+        } else {
+            for (i = 0; i < NUM_CANNONS; i++) {
+                bulletYes[i] = cannonYes[i];
+                bulletX[i] = initBulletX[i];
+                bulletY[i] = initBulletY[i];
+                for (j = 0; j < DIAGONAL_LENGTH; j++) {
+                    xPathBullet[i][j] = -1;
+                    yPathBullet[i][j] = -1;
+                }
             }
+            count = 0;
         }
         drawCannonLeftEdge(cannonX[0], cannonY[0], cannonDirections[0], cannonYes[0]);
         drawCannonLeftEdge(cannonX[1], cannonY[1], cannonDirections[1], cannonYes[1]);
@@ -213,24 +314,28 @@ int main(void) {
         drawCannonBottomEdge(cannonX[8], cannonY[8], cannonDirections[8], cannonYes[8]);
         drawCannonBottomEdge(cannonX[9], cannonY[9], cannonDirections[9], cannonYes[9]);
 
-
-        drawBullet(150, 150, 1);
-
-
-        int j;
-//        int k;
-        for (i = 0; i < playerSize && !dead; i++) {
-            for (j = 0; j < playerSize && !dead; j++) {
-//                printf("px:%d, py:%d, i:%d, j:%d, d:%d\n", playerX, playerY, i, j, display[playerY + i][playerX + j]);
-                if (display[playerY + i][playerX + j]) {
-                    dead = 1;
-//                    for (k = 0; k < playerSize; k++) {
-//                        draw_line(playerX, playerY + k, playerX + playerSize - 1, playerY + k, MAGENTA);
-//                    }
-                    printf("Dead at: (%d, %d)", playerX + j, playerY + i);
+        if (canDie) {
+            int shouldBeDead = 0;
+            for (i = 0; i < playerSize && !dead; i++) {
+                for (j = 0; j < playerSize && !dead; j++) {
+//                printf("px:%d, py:%d, i:%d, j:%d, d:%d\n", playerX, playerY, i, j, displayFront[playerY + i][playerX + j]);
+                    if (!back) {
+                        shouldBeDead = displayBack[playerY + i][playerX + j];
+                    } else {
+                        shouldBeDead = displayFront[playerY + i][playerX + j];
+                    }
+                    if (shouldBeDead) {
+                        dead = 1;
+                        for (k = 0; k < playerSize; k++) {
+                            draw_line(playerX, playerY + k, playerX + playerSize - 1, playerY + k, MAGENTA);
+                        }
+                        printf("Dead at: (%d, %d)", playerX + j, playerY + i);
+                    }
                 }
             }
         }
+
+
         if (playerX <= 0 && dxPlayer < 0) {
             dxPlayer = 1;
         } else if (playerX >= RESOLUTION_X - 1 - playerSize && dxPlayer > 0) {
@@ -247,9 +352,13 @@ int main(void) {
         if (dead) {
             if (!drawing) {
                 FILE *f = fopen("test.txt", "w");
-                for (i = 0; i < RESOLUTION_X; i++) {
-                    for (j = 0; j < RESOLUTION_Y; j++) {
-                        fprintf(f, "%d ", display[i][j]);
+                for (i = 0; i < RESOLUTION_Y; i++) {
+                    for (j = 0; j < RESOLUTION_X; j++) {
+                        if (back) {
+                            fprintf(f, "%d ", displayBack[i][j]);
+                        } else {
+                            fprintf(f, "%d ", displayFront[i][j]);
+                        }
                     }
                     fprintf(f, "\n");
                 }
@@ -318,25 +427,12 @@ void draw_line(int x0, int y0, int x1, int y1, short int colour) {
 //            }
         } else {
             plot_pixel(x, y, colour);
-            // add pixel to corresponding drewPixel
-//            if (back) {
-//                drewPixelBack[ndrewPixelBack][0] = x;
-//                drewPixelBack[ndrewPixelBack][1] = y;
-//            } else {
-//                drewPixelFront[ndrewPixelFront][0] = x;
-//                drewPixelFront[ndrewPixelFront][1] = y;
-//            }
         }
         error += dY;
         if (error > 0) {
             y += y_step;
             error -= dX;
         }
-//        if (back) {
-//            ndrewPixelBack += 1;
-//        } else {
-//            ndrewPixelFront += 1;
-//        }
     }
 }
 
@@ -388,14 +484,25 @@ void plot_pixel(int x, int y, short int line_color) {
             ndrewPixelFront += 1;
         }
     }
-    //display[y][x] = line_color;
     if (line_color == (short) WHITE || line_color == (short) cPlayer || line_color == (short) cPlayerDeco ||
         line_color == (short) cPlayerBorder) {
-        display[y][x] = 0;
+        if (back) {
+            displayBack[y][x] = 0;
+        } else {
+            displayFront[y][x] = 0;
+        }
     } else if (line_color == (short) MAGENTA) {
-        display[y][x] = 2;
+        if (back) {
+            displayBack[y][x] = 2;
+        } else {
+            displayFront[y][x] = 2;
+        }
     } else {
-        display[y][x] = 1;
+        if (back) {
+            displayBack[y][x] = 1;
+        } else {
+            displayFront[y][x] = 1;
+        }
     }
 
     if (drawing)
@@ -526,7 +633,6 @@ void drawPlayer(int x, int y) {
 
 void drawBullet(int x, int y, int appear) {
     if (appear) {
-        int bulletSize = cannonSize - 1;
         int i;
         for (i = 0; i < bulletSize; i++) {
             draw_line(x, y + i, x + bulletSize - 1, y + i, cBullet);
@@ -556,6 +662,35 @@ void drawBullet(int x, int y, int appear) {
             }
         }
     }
+}
+
+void computeBulletPath(int bulletX, int bulletY, int playerX, int playerY, int bulletIdx,
+                       int bulletPathX[NUM_CANNONS][DIAGONAL_LENGTH],
+                       int bulletPathY[NUM_CANNONS][DIAGONAL_LENGTH], int speed) {
+    double dx = playerX - bulletX;
+    double dy = playerY - bulletY;
+    double l = sqrt(pow(dx, 2) + pow(dy, 2));
+    dx = dx / l * speed;
+    dy = dy / l * speed;
+    double x = bulletX + dx;
+    double y = bulletY + dy;
+    int i = 0;
+    while (x <= RESOLUTION_X - bulletSize && y <= RESOLUTION_Y - bulletSize && x >= 0 && y >= 0) {
+        bulletPathX[bulletIdx][i] = round(x);
+        bulletPathY[bulletIdx][i] = round(y);
+        x = x + dx;
+        y = y + dy;
+        i++;
+    }
+}
+
+int countOccurances(int arr[], int n, int item) {
+    int i, o = 0;
+    for (i = 0; i < n; i++) {
+        if (arr[i] == item)
+            o++;
+    }
+    return o;
 }
 
 #pragma clang diagnostic pop
